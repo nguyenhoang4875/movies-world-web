dd3550e3e0a1afb3fc475ee54588d93d0aeb3cf7
quynhnhi144 <ltqnhi97@gmail.com>
2020-06-20 14:38:09 +0700

    delete show time film component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,6 @@ import { StaffEditComponent } from "./staffs/staff-edit/staff-edit.component";
 import { StaffListComponent } from "./staffs/staff-list/staff-list.component";
 import { StaffService } from "./staffs/staff.service";
 import { MovieService } from "./movies/movie.service";
-import { ShowTimeFilmComponent } from './show-time-film/show-time-film.component';
 
 @NgModule({
   declarations: [
@@ -71,7 +70,6 @@ import { ShowTimeFilmComponent } from './show-time-film/show-time-film.component
     PlaceholderDirective,
     AlertComponent,
     ToastsComponent,
-    ShowTimeFilmComponent,
   ],
   imports: [
     BrowserModule,
